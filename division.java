6bf14c261ee04195259edb0cb4833a7e992eed24
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2023-12-10 03:33:35 +0530

    Added implmentation for division.java

--- division.java
+++ division.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class division {
     public static void main(String[] args) {
-        
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int num1=sc.nextInt();
+        System.out.println("Enter second number: ");
+        int num2=sc.nextInt();
+        int result=num1/num2;
+        System.out.println("The Division of two number is:"+result);
     }
 }
