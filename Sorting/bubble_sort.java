d672c9e81ddc4fdd5b710be9aabc3f4f62a2e9a9
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2024-04-04 21:02:18 +0530

    Bubble Sort

--- Sorting/bubble_sort.java
+++ Sorting/bubble_sort.java
@@ -15,7 +15,7 @@ public class bubble_sort {
         System.out.println("Enter size of the array");  
         int n = sc.nextInt();
         int arr[] = new int[n];
-        System.out.println("Enter "+n+" elements :");
+        System.out.println("Enter "+ n +" elements :");
         for(int i=0; i<n;i++)
         {
             arr[i]=sc.nextInt();
