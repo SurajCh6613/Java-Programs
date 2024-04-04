a23556c1f15cf00dcd0a28b979f123a5aa2babf9
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2024-04-04 20:40:07 +0530

    Array initialized

---
+++ Sorting/bubble_sort.java
@@ -0,0 +1,21 @@
+package Sorting;
+
+import java.util.Scanner;
+
+public class bubble_sort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // //  To manually enter array element
+        // System.out.println("Enter size of the array");  
+        // int n = sc.nextInt();
+        // int arr[] = new int[n];
+        // for(int i=0; i<n;i++)
+        // {
+        //     arr[i]=sc.nextInt();
+        // }
+
+        int arr[]  = new int[]{7,8,4,5,2,1,3};
+    
+    }
+}
